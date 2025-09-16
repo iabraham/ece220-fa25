7f02edf265c628501ddfd662b990592a4d6cbf2f
Ivan Abraham <ivan.abrms@gmail.com>
2025-09-16 10:37:19 -0500

    Prelecture on 09/16

---
+++ lec06_0911/runnables/print_id.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+void print_id(int size){
+  int i, j; 
+  for (i=0; i<size; i++){ // Looping over rows
+    for (j=0; j<size; j++){  // Looping over columns
+      if (i==j)
+        printf("1\t");
+      else if (i<j)
+        printf("0\t");
+      else
+        printf("\t");
+    }
+    printf("\n");
+  }
+}
+
+int main(){
+  int size;
+  printf("What size:\t");
+  scanf("%d", &size);
+  if (size >=1 && size <=8)
+    print_id(size);
+  else
+    printf("Invalid dimensions!");
+}
