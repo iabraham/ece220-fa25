374cb06d2df046fbb63c11692e4254ac8c8c0d50
Ivan Abraham <ivan.abrms@gmail.com>
2025-09-18 10:37:21 -0500

    Prelecture on 09/18

--- lec06_0911/static_var.c
+++ lec06_0911/static_var.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-void printx(){
-	static int x = 0;
-	x++;
-	printf("value of x is %d \n",x);
+void printx() {
+  static int x = 0;
+  x++;
+  printf("value of x is %d \n", x);
 }
 
-int main(){
-	printx();
-	printx();
-	printx();
-	printx();
+int main() {
+  printx();
+  printx();
+  printx();
+  printx();
 
-	return 0;
+  return 0;
 }
--- lec07_0916/temp_conv.c
+++ lec07_0916/temp_conv.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
-float f2c(int temp_f){
-  return (temp_f - 32)*5.0/9.0;
-}
+float to_celsius(int ftemp) { return (ftemp - 32) * 5.0 / 9.0; }
 
-
-int main(void){
-  int temp_f; 
+int main(void) {
+  int temp_f;
   float temp_c;
   do {
-  printf("Enter temperature in F:\t");
-  scanf("%d", &temp_f);
-  temp_c = f2c(temp_f);
-  printf("Value in Celsius is: %f", temp_c);
+    printf("Enter temperature in F:\t");
+    scanf("%d", &temp_f);
+    temp_c = to_celsius(temp_f);
+    printf("Value in Celsius is: %f", temp_c);
   } while (temp_c > -273.15f);
 }
