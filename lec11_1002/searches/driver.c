65a273dcb6308b205da5818ecd0a6bfb7747408f
Ivan Abraham <ivan.abrms@gmail.com>
2025-10-02 16:51:19 -0500

    Fix driver code to show output

--- lec11_1002/searches/driver.c
+++ lec11_1002/searches/driver.c
@@ -6,14 +6,15 @@ int main(void) {
   int arr[] = {17, 43, 9, 40, 11, 68, 13};
   int len = sizeof(arr) / sizeof(arr[0]);
   int i, idx;
+  int key = 40;
 
   for (i = 0; i < len; i++)
     printf("%d ", arr[i]);
 
   printf("\n");
 
-  idx = search(arr, len, 40);
+  idx = search(arr, len, key);
 
-  for (i = 0; i < len; i++)
-    printf("%d ", arr[i]);
+  if (idx >= 0)
+    printf("Found %d at index %d with value %d\n", key, idx, arr[idx]);
 }
