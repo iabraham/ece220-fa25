371987ee93119fb9f6204309c799328ea6bca2ec
Ivan Abraham <ivan.abrms@gmail.com>
2025-10-16 10:22:03 -0500

    Prelecture on 10/16

---
+++ lec15_1016/read_flights.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef struct geoloc {
+  float lattitude;
+  float longitude;
+} geo;
+
+typedef struct flight {
+  char code[8];
+  unsigned int arrival_time;
+  unsigned int depart_time;
+  geo origin;
+  geo destination;
+} Flight;
+
+int main(void) { Flight flights[10]; }
