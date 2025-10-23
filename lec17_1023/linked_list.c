389dead0fef7dcc1851f05c7d6bb4fc8f60a1bc0
Ivan Abraham <ivan.abrms@gmail.com>
2025-10-23 12:33:37 -0500

    Post lecture on 10/23

--- lec17_1023/linked_list.c
+++ lec17_1023/linked_list.c
@@ -8,16 +8,48 @@ typedef struct person {
 } node;
 
 // Code to print list - EX 1 (Slide 18)
-void print_list(node *cursor) {}
+void print_list(node *cursor) {
+  if (cursor == NULL) {
+    return;
+  } else {
+    printf("%s was born in %d\n", cursor->name, cursor->byear);
+    print_list(cursor->next);
+  }
+}
 
 // Code to add at head - EX 1 (Slide 18)
-void add_at_head(node **cursor, node *new) {}
+void add_at_head(node **cursor, node *new) {
+  node *temp = malloc(sizeof(node));
+  temp->name = new->name;
+  temp->byear = new->byear;
+  temp->next = new->next;
+
+  if (*cursor == NULL) {
+    *cursor = temp;
+  } else {
+    temp->next = *cursor;
+    *cursor = temp;
+  }
+}
 
 // Code to add at tail - (Slide 23)
-void add_at_tail(node **cursor, node *new) {}
+void add_at_tail(node **cursor, node *new) {
+  if (*cursor == NULL)
+    add_at_head(cursor, new);
+  else
+    add_at_tail(&(*cursor)->next, new);
+}
 
 // Code to delete at head - (Slide 25)
-void del_head(node **headptr) {}
+void del_head(node **headptr) {
+  if (*headptr == NULL)
+    return;
+  else {
+    node *old_head = *headptr;
+    *headptr = (*headptr)->next;
+    free(old_head);
+  }
+}
 
 // Code to destroy list - (Slide 31)
 void destroy(node **headptr) {
@@ -28,7 +60,20 @@ void destroy(node **headptr) {
 }
 
 // Code to delete at tail - (Slide 26)
-void del_tail(node **cursor) {}
+void del_tail(node **cursor) {
+  if (*cursor == NULL)
+    return;
+  if ((*cursor)->next == NULL) {
+    free(*cursor);
+    *cursor = NULL;
+    return;
+  }
+  node *second_last = *cursor;
+  while (second_last->next->next != NULL)
+    second_last = second_last->next;
+  free(second_last->next);
+  second_last->next = NULL;
+}
 
 int main(void) {
   char *names[] = {"Alex", "John", "Mary", "Sue"};
