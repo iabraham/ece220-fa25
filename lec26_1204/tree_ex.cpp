f439ffd0e7ec3c02224d00ccca38f18fb8e2d436
itabrah2-eceb2058 <ivan.abrms@gmail.com>
2025-12-09 09:36:57 -0600

    Prelecture on 12/09

--- lec26_1204/tree_ex.cpp
+++ lec26_1204/tree_ex.cpp
@@ -2,50 +2,50 @@
 #include <iostream>
 
 /* Template the function so that it can be used with any data type */
-struct node {
-  int data;
-  struct node *left;
-  struct node *right;
-  node() : data(0), left(NULL), right(NULL) {};
-  node(int d) : data(d), left(NULL), right(NULL) {};
-  node(int d, node *l, node *r) : data(d), left(l), right(r) {};
+template <typename T> struct node {
+  T data;
+  struct node<T> *left;
+  struct node<T> *right;
+  //  node() : data(0), left(NULL), right(NULL) {};
+  node(T d) : data(d), left(nullptr), right(nullptr) {};
+  node(T d, node<T> *l, node<T> *r) : data(d), left(l), right(r) {};
 };
 
 /* Template the function so that it can be used with any data type */
-void tree_print(node *cursor, int depth) {
+template <typename T> void tree_print(node<T> *cursor, int depth) {
   if (cursor == NULL)
     return;
   for (int i = 0; i < depth; i++)
     printf(i == depth - 1 ? "|-" : "  ");
-  printf("%d\n", cursor->data);
+  //  printf("%d\n", cursor->data);
+  cout << cursor->data << endl;
   tree_print(cursor->left, depth + 1);
   tree_print(cursor->right, depth + 1);
 }
 
-// typedef int<char> inode;
-// typedef node<char> inode;
+/* Modify main function appropriately to test the templated tree structure
 
-/* Modify main function appropriately to test the templated tree structure */
-
+typedef node<char> inode;
 int main() {
-  node *left, *right;
+  inode *left, *right;
 
-  left = new node(6);
-  right = new node(7, left, NULL);
-  left = new node(4);
-  left = new node(5, left, right);
+  left = new inode(65);
+  right = new inode(67, left, NULL);
+  left = new inode(74);
+  left = new inode(65, left, right);
 
-  right = new node(9);
-  right = new node(8, left, right);
+  right = new inode(69);
+  right = new inode(68, left, right);
 
-  left = new node(1);
-  left = new node(2, left, NULL);
-  node *root = new node(3, left, right);
+  left = new inode(71);
+  left = new inode(72, left, NULL);
+  inode *root = new inode(73, left, right);
 
   tree_print(root, 0);
 }
 
-/*
+*/
+
 typedef node<Person> inode;
 
 int main() {
@@ -64,4 +64,3 @@ int main() {
 
   tree_print(root, 0);
 }
-*/
